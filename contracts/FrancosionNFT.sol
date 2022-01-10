369b0b79416d8365b798abee3a60e60c2d4a9c13
Anthony Nwobodo <anthony@Anthonys-MacBook-Pro.local>
2022-01-10 17:01:27 +0100

    created a mint function

--- contracts/FrancosionNFT.sol
+++ contracts/FrancosionNFT.sol
@@ -3,8 +3,33 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-contract FrancosionNFT {
-    constructor () {
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract FrancosionNFT is ERC721URIStorage {
+    // Magic given to us by OpenZeppelin to help us keep track of tokenIds.
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor () ERC721 ("Francosion NFT", "FNFT") {
         console.log('Hello, this is Francosion NFT');
+    }
+
+     // A function our user will hit to get their NFT.
+    function mintNFT() public {
+        // Get the current tokenId, this starts at 0.
+        uint256 newItemId = _tokenIds.current();
+
+        // Actually mint the NFT to the sender using msg.sender.
+        _safeMint(msg.sender, newItemId);
+
+        // Set the NFTs data.
+        _setTokenURI(newItemId, 'https://gateway.pinata.cloud/ipfs/QmReH3b5GX2uMJh54zPuMzPBhHJSXkqL2ZVN1rjbsBR6pg/FNFT.json');
+
+        // Increment the counter for when the next NFT is minted.
+        _tokenIds.increment();
+
+        console.log("An NFT with ID %s has been minted to %s", newItemId, msg.sender);
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,10 +1,20 @@
-const hre = require("hardhat");
+const hre = require('hardhat')
 
-async function main() {
-  const nftContractFactory = await hre.ethers.getContractFactory('FrancosionNFT');
-  const nftContract = await nftContractFactory.deploy();
-  await nftContract.deployed();
-  console.log("Contract deployed to:", nftContract.address);
+async function main () {
+  const nftContractFactory = await hre.ethers.getContractFactory('FrancosionNFT')
+  const nftContract = await nftContractFactory.deploy()
+  await nftContract.deployed()
+  console.log('Contract deployed to:', nftContract.address)
+
+  // Call the function.
+  let txn = await nftContract.mintNFT()
+  // Wait for it to be mined.
+  await txn.wait()
+
+  // Mint another NFT for fun.
+  txn = await nftContract.mintNFT()
+  // Wait for it to be mined.
+  await txn.wait()
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -12,6 +22,6 @@ async function main() {
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    console.error(error)
+    process.exit(1)
+  })
