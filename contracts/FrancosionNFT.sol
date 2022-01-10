391e51460f9df295a45fe0a592e9d796cd69d282
Anthony Nwobodo <anthony@Anthonys-MacBook-Pro.local>
2022-01-10 06:31:12 +0100

    project setup

---
+++ contracts/FrancosionNFT.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract FrancosionNFT {
+    constructor () {
+        console.log('Hello, this is Francosion NFT');
+    }
+}
---
+++ scripts/run.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+  const nftContractFactory = await hre.ethers.getContractFactory('FrancosionNFT');
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
