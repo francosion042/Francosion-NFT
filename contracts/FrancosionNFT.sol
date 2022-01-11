a699a62f546a1b8995e874d39d29be93465d3d75
Anthony Nwobodo <anthony@Anthonys-MacBook-Pro.local>
2022-01-11 22:53:33 +0100

    made onchain

--- contracts/FrancosionNFT.sol
+++ contracts/FrancosionNFT.sol
@@ -25,7 +25,7 @@ contract FrancosionNFT is ERC721URIStorage {
         _safeMint(msg.sender, newItemId);
 
         // Set the NFTs data.
-        _setTokenURI(newItemId, 'https://gateway.pinata.cloud/ipfs/QmReH3b5GX2uMJh54zPuMzPBhHJSXkqL2ZVN1rjbsBR6pg/FNFT.json');
+        _setTokenURI(newItemId, "data:application/json;base64,ewogICAgIm5hbWUiOiAiRnJhbmNvc2lvbiBORlQiLAogICAgImRlc2NyaXB0aW9uIjogIklnYm8gQXJ0LCBhbiBJZ2JvIEV4dGVuZGVkIGZhbWlseSBlYXRpbmcuIiwKICAgICJpbWFnZSI6ICJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUhkb2FYUmxPeUJtYjI1MExXWmhiV2xzZVRvZ2MyVnlhV1k3SUdadmJuUXRjMmw2WlRvZ01UUndlRHNnZlR3dmMzUjViR1UrQ2lBZ0lDQThjbVZqZENCM2FXUjBhRDBpTVRBd0pTSWdhR1ZwWjJoMFBTSXhNREFsSWlCbWFXeHNQU0ppYkdGamF5SWdMejRLSUNBZ0lEeDBaWGgwSUhnOUlqVXdKU0lnZVQwaU5UQWxJaUJqYkdGemN6MGlZbUZ6WlNJZ1pHOXRhVzVoYm5RdFltRnpaV3hwYm1VOUltMXBaR1JzWlNJZ2RHVjRkQzFoYm1Ob2IzSTlJbTFwWkdSc1pTSStSbkpoYm1OdmMybHZiaUJPUmxROEwzUmxlSFErQ2p3dmMzWm5QZz09Igp9");
 
         // Increment the counter for when the next NFT is minted.
         _tokenIds.increment();
